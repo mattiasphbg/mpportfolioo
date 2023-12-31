a23f0ddc4035b146b224939d960e8005d235997a
mattiasphbg <mattiasp@hotmail.com>
2023-12-31 15:23:47 +0100

    fix: removed optional Z hireMESchema

--- src/app/schemas/form.ts
+++ src/app/schemas/form.ts
@@ -3,15 +3,12 @@
 import * as z from "zod";
 
 export const HireMeSchema = z.object({
-  name: z.string().optional(),
-  companyName: z.string().optional(),
-  phoneNumber: z
-    .string()
-    .refine((val) => val.length === 10, {
-      message: "Phone number must be 10 digits long",
-    })
-    .optional(),
-  email: z.string().email().optional(),
+  name: z.string(),
+  companyName: z.string(),
+  phoneNumber: z.string().refine((val) => val.length === 10, {
+    message: "Phone number must be 10 digits long",
+  }),
+  email: z.string().email(),
   areaOfInterest: z.string().optional(),
-  message: z.string().optional(),
+  message: z.string(),
 });
