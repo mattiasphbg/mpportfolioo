b4b727a03b409a887504f3c1b616cb34784ae68f
mattiasphbg <mattiasp@hotmail.com>
2024-01-07 17:13:09 +0100

    fix:  This assertion is unnecessary since it does not change the type of the expression.  @typescript-eslint/no-unnecessary-type-assertion

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -1,7 +1,6 @@
 import { ProjectById } from "src/components/project/projectById";
 
 export default function Page({ params }: { params: { id: string } }) {
-  const isProduction = process.env.NODE_ENV === "production";
   return (
     <>
       <ProjectById id={`${params.id}`} />
