29e44aa35b5b5c2ea0466c6c8ed5cd4c3dca807c
mattiasphbg <mattiasp@hotmail.com>
2024-11-29 13:04:25 +0100

    refactor: add drizzle and supabase to project procedure so we remove the cold start;

--- src/server/api/routers/projects.ts
+++ src/server/api/routers/projects.ts
@@ -7,55 +7,7 @@ import { db } from "~/db/index";
 import { mpPortfolio } from "src/db/schema";
 
 export const projects = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.db.portfolio_Project.findMany();
+  getAll: publicProcedure.query(({}) => {
+    return db.select().from(mpPortfolio);
   }),
-  getOne: publicProcedure
-    .input(z.object({ id: z.string() }))
-    .query(({ ctx, input }) => {
-      const { id } = input;
-      const project = ctx.db.mpPortfolio
-        .findUnique({
-          where: { id },
-        })
-        .catch((err) => console.error(err));
-
-      return project;
-    }),
-  add: publicProcedure
-    .input(
-      z.object({
-        title: z.string().min(1),
-        description: z.string().optional().default(""),
-        img: z.string().optional().default(""),
-      }),
-    )
-    .mutation(async ({ ctx, input }) => {
-      const add = await ctx.db.portfolio_Project.create({
-        data: {
-          title: input.title,
-          description: input.description || "",
-          img: input.img || "",
-        },
-      });
-      return add;
-    }),
-  getMultiple: publicProcedure
-    .input(z.array(z.string()).optional())
-    .query(({ ctx, input = [] }) => {
-      const projectIds = input;
-      if (projectIds.length === 0) {
-        return [];
-      }
-
-      const projects = ctx.db.portfolio_Project.findMany({
-        where: {
-          id: {
-            in: projectIds,
-          },
-        },
-      });
-
-      return projects;
-    }),
 });
