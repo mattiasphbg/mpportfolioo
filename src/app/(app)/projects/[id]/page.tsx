5c8a61fae887ddc260aaac9f44378a9203f8ecb0
mattiasphbg <mattiasp@hotmail.com>
2024-12-03 10:38:21 +0100

    ref: Change page parameter type to number;

--- src/app/(app)/projects/[id]/page.tsx
+++ src/app/(app)/projects/[id]/page.tsx
@@ -1,9 +1,9 @@
 import { ProjectById } from "src/components/project/projectById";
 
-export default function Page({ params }: { params: { id: string } }) {
+export default function Page({ params }: { params: { id: number } }) {
   return (
     <>
-      <ProjectById id={`${params.id}`} />
+      <ProjectById id={params.id} />
     </>
   );
 }
